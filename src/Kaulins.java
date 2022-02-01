ec68269a57a62dc480487073a680b5db8f8915ad
pauls <gustavspauls1@gmail.com>
2022-02-01 12:22:20 +0200

    Integreta izvele

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -13,12 +13,23 @@ public class Kaulins {
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int reizes;
+		int reizes, izvele;
+		do {
+			System.out.println("1-Mest kaulinu | 2-Apturet");
+			izvele = scan.nextInt();
+			switch(izvele) {
+			case 1:
 		do {
 			System.out.println("Cik reizes mest kauliņu?");
 			reizes = scan.nextInt();
 		}while(reizes<1);
-		mestKaulinu(reizes);	
+		mestKaulinu(reizes);
+		break;
+			case 2:
+				System.out.println("Programma aptureta");
+				break;
+			}
+		}while(izvele!=2);
 		scan.close();
 	}
 }
